26613e96a902e99899d05a0427ab62d6223ea571
Mridula Garg <mgarg425@gmail.com>
2018-03-16 02:18:58 +0530

    Add files via upload

---
+++ Animation/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.animation.frameanimation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnimationDrawable batAnimation;
+    private ImageView batImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batImage = (ImageView) findViewById(R.id.batID);
+       // batImage.setBackgroundResource(R.drawable.bat_anim);
+        //batAnimation = (AnimationDrawable) batImage.getBackground();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        //batAnimation.start();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein_animation);
+                batImage.startAnimation(startAnimation);
+
+                //stop the animation
+                //batAnimation.stop();
+            }
+        }, 50);  //miliseconds
+
+        return super.onTouchEvent(event);
+    }
+}
