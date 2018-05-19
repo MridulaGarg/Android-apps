25991fb87a49aa796d0aae1f1a62af01438503d8
Mridula Garg <mgarg425@gmail.com>
2018-05-19 17:10:32 +0530

    Update Constants.java

--- Movie Directory/Util/Constants.java
+++ Movie Directory/Util/Constants.java
@@ -2,7 +2,7 @@ package app.directoy.movie.moviedirectory.Util;
 
 public class Constants {
 
-    public static final String URL_LEFT = "http://www.omdbapi.com/?apikey=b1910637&s=";
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
     public static final String URL_RIGHT = "&page=2";
-    public static final String URL = "http://www.omdbapi.com/?apikey=b1910637&i=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
 }
